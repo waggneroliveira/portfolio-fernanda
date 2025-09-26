b6e6cfb0ce3d7167f6c6d82c6cee701e3ef4456e
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2025-09-26 11:36:53 -0300

    Update

--- resources/views/client/includes/lgpd/lgpd.blade.php
+++ resources/views/client/includes/lgpd/lgpd.blade.php
@@ -1,6 +1,6 @@
     <div id="lgpd-container">
-        <div id="lgpd-banner" style="background-color:#10131C;">
+        <div id="lgpd-banner">
             <span class="rethink-sans-semiBold font-15">Utilizamos cookies para melhorar sua experiência. Ao continuar navegando, você concorda com nossa <a href="https://policies.google.com/privacy?hl=pt-BR" target="_blank" rel="noopener noreferrer" class="text-red">Política de Privacidade</a>.</span>
-            <button id="accept-btn" class="background-red rounded-2 rethink-sans-semiBold font-16" style="padding:10px 60px;">Aceitar</button>
+            <button id="accept-btn" class="background-red rounded-2 rethink-sans-semiBold font-16">Aceitar</button>
         </div>
     </div>
