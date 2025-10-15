76733f74ad830509f33e6fe2543507a4c35bac25
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2025-10-15 10:48:25 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -60,77 +60,26 @@ Route::get('/senha-alterada-com-sucesso', function () {
     return view('emails.password-success-client-reset');
 })->name('client-success-reset-password');
 
-
-Route::middleware([AuthClientMiddleware::class])->group(function () {
-    Route::put('/client/update', [ClientController::class, 'update'])->name('client.update');
-
-    Route::post('/client/comments', [CommentController::class, 'store'])
-    ->name('blog.comment');
-
-    Route::get('logout', [AuthClientController::class, 'logout'])->name('client.user.logout');
-});
-
 Route::get('contato', [ContactPageController::class, 'index'])
 ->name('contact');
 Route::post('send-contact', [FormIndexController::class, 'store'])->name('send-contact');
 Route::get('projeto/{slug}', [ProjectPageController::class, 'project'])
 ->name('project');
-Route::get('blog/interna/{slug}', [BlogPageController::class, 'blogInner'])
-->name('blog-inner');
-Route::get('blog/{category?}', [BlogPageController::class, 'index'])->name('blog');
-Route::post('blog/search', [BlogPageController::class, 'index'])->name('blog-search');
 Route::post('send-newsletter', [NewsletterController::class, 'store'])->name('send-newsletter');
 
 Route::post('cliente/cadastro', [ClientController::class, 'store'])->name('register-client');
 Route::get('home', [HomePageController::class, 'index'])->name('index');
 Route::get('sobre', [AboutPageController::class, 'index'])->name('about');
-// Route::get('servicos-aos-sindicalizados', [BenefitPageController::class, 'index'])->name('unionized');
 Route::get('portfolio', [ProjectPageController::class, 'portfolio'])->name('portfolio');
-// Route::get('juridico/search', [JuridicoPageController::class, 'searchJuridico'])->name('search-juridico');
-// Route::get('regionais', [RegionPageController::class, 'index'])->name('regional');
-// Route::match(['get', 'post'], 'regionais/filter-municipalities', [RegionPageController::class, 'filterMunicipalities'])
-// ->name('client.filter.municipalities');
 
-
-// Route::get('agenda', [EventPageController::class, 'index'])->name('client.event');
 Route::get('home/blog/filter/{category?}', [HomePageController::class, 'filterByCategory'])
     ->name('blog.filter');
 
 
 View::composer('client.core.client', function ($view) {
-    $blogCategories = BlogCategory::whereHas('blogs')
-    ->active()
-    ->sorting()
-    ->limit(10)
-    ->get();
-    $announcements = Announcement::select(
-        'exhibition',
-        'link',
-        'exhibition',
-        'path_image',
-        'active',
-        'sorting',
-    )
-    ->where('exhibition', '=', 'mobile')
-    ->orWhere('exhibition', '=', 'horizontal')
-    ->active()
-    ->sorting()
-    ->get();
     $contact = Contact::first();
     $abouts = About::active()->sorting()->get();
-    $statute = Statute::active()->count();
-    $directions = Direction::active()->sorting()->count();
-    $benefitTopics = BenefitTopic::active()->sorting()->count();
-    $report = Report::active()->count();
-    $agreement = Agreement::active()->count();
 
-    return $view->with('blogCategories', $blogCategories)
-    ->with('announcements', $announcements)
-    ->with('contact', $contact)
-    ->with('statute', $statute)
-    ->with('directions', $directions)
-    ->with('benefitTopics', $benefitTopics)
-    ->with('report', $report)
-    ->with('agreement', $agreement)
+    return $view->with('contact', $contact)
     ->with('abouts', $abouts);
 });
